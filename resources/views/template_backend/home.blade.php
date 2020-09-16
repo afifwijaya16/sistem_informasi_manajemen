26bb4891dd8c9ed2cbc2b52f310d47e99e5911d3
afwjy16 <afif0071997@gmail.com>
2020-09-16 15:06:24 +0700

    first commit

---
+++ resources/views/template_backend/home.blade.php
@@ -0,0 +1,29 @@
+@include('template_backend/header')
+@include('template_backend/navbar')
+@include('template_backend/aside')
+
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark"></h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">@yield('sub-breadcrumb')</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+    </section>
+</div>
+
+@include('template_backend/footer')
+@include('template_backend/javascript')
