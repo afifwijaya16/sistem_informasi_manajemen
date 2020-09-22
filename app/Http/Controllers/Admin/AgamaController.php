5e06a38bc586aeba98a9bfef76267c71bd8738f8
afwjy16 <afif0071997@gmail.com>
2020-09-22 14:53:42 +0700

    auth

--- app/Http/Controllers/Admin/AgamaController.php
+++ app/Http/Controllers/Admin/AgamaController.php
@@ -13,6 +13,11 @@ class AgamaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         // $agama = AgamaModel::all();
--- app/Http/Controllers/Admin/PekerjaanController.php
+++ app/Http/Controllers/Admin/PekerjaanController.php
@@ -13,6 +13,11 @@ class PekerjaanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         $pekerjaan = PekerjaanModel::paginate(5);
